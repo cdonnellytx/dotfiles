39649af0f9f2d0f494fddc67187408700b29608d
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-13 12:41:49 -0600

    Trust the fungus

---
+++ home/.chezmoiscripts/windows/run_once_before_01-init-pwsh.ps1
@@ -0,0 +1,43 @@
+#requires -Version 7 -Modules Microsoft.PowerShell.PSResourceGet
+
+[CmdletBinding(SupportsShouldProcess)]
+param()
+
+function Confirm-PSResourceRepositoryTrusted
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    param
+    (
+        [Parameter(Position = 0, Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string[]] $Name
+    )
+
+    begin
+    {
+        Write-Header "Confirm trusted PSResourceRepository"
+    }
+
+    process
+    {
+        Get-PSResourceRepository -Name:$Name | ForEach-Object {
+            Write-Operation "Trusting $($_.Name)..."
+            if ($_.Trusted)
+            {
+                Write-Result "OK (already trusted)"
+                return
+            }
+
+            $_ | Set-PSResourceRepository -Trusted -WhatIf:$WhatIfPreference -ErrorVariable err
+            if ($err)
+            {
+                Write-Result -Err:$err
+            }
+            else
+            {
+                Write-Result "OK"
+            }
+        }
+    }
+}
+
+Confirm-PSResourceRepositoryTrusted -Name 'PSGallery'
