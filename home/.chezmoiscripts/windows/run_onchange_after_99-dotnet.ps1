aac41f18e9bfebaf49187df190b13259157102ce
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-16 11:51:15 -0600

    .NET post-install settings

---
+++ home/.chezmoiscripts/windows/run_onchange_after_99-dotnet.ps1
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S pwsh
+#requires -version 7 -modules bootstrap.environment, bootstrap.ux
+[CmdletBinding(SupportsShouldProcess)]
+param()
+
+$commonParams = $PSBoundParameters
+
+<#
+.SYNOPSIS
+Disable checking for workflow updates on running `dotnet`.
+
+.LINK
+https://github.com/dotnet/sdk/issues/22571
+.LINK
+https://stackoverflow.com/a/79073472/17152
+#>
+function Disable-WorkloadUpdateNotify
+{
+    # GITCRAP: Because I symlink %APPDATA%\git to ~\.config\git, Git warns:
+    # warning: 'C:\Users\cdonnelly/.config/git/config' was ignored because 'C:\Users\cdonnelly\AppData\Roaming/Git/config' exists.
+    # Setting GIT_CONFIG_GLOBAL works around this.
+    Enter-Operation 'Set DOTNET_CLI_WORKLOAD_UPDATE_NOTIFY_DISABLE'
+    Set-EnvironmentVariable -Name 'DOTNET_CLI_WORKLOAD_UPDATE_NOTIFY_DISABLE' -Value 'true' -Target User @commonParams -ErrorVariable:err
+    Exit-Operation -Error:$err
+}
+
+<#
+.SYNOPSIS
+Disable checking for workflow updates on running `dotnet`.
+
+.LINK
+https://github.com/dotnet/sdk/issues/22571
+.LINK
+https://stackoverflow.com/a/79073472/17152
+#>
+function Disable-Telemetry
+{
+    # GITCRAP: Because I symlink %APPDATA%\git to ~\.config\git, Git warns:
+    # warning: 'C:\Users\cdonnelly/.config/git/config' was ignored because 'C:\Users\cdonnelly\AppData\Roaming/Git/config' exists.
+    # Setting GIT_CONFIG_GLOBAL works around this.
+    Enter-Operation 'Set DOTNET_CLI_TELEMETRY_OPTOUT'
+    Set-EnvironmentVariable -Name 'DOTNET_CLI_TELEMETRY_OPTOUT' -Value 'true' -Target User @commonParams -ErrorVariable:err
+    Exit-Operation -Error:$err
+}
+
+#
+#
+#
+
+Disable-Telemetry
+Disable-WorkloadUpdateNotify
