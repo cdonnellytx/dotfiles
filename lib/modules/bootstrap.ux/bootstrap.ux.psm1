ea79e3bf784f1b15d3238430ec9acb2f48a11b09
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-17 15:50:31 -0600

    Fixing width

--- lib/modules/bootstrap.ux/bootstrap.ux.psm1
+++ lib/modules/bootstrap.ux/bootstrap.ux.psm1
@@ -94,7 +94,7 @@ function Enter-Operation
         [string] $Name
     )
 
-    $padding = [Math]::Max(150, [Math]::Min(60, $host.UI.RawUI.WindowSize.Width - 20))
+    $padding = [Math]::Clamp($host.UI.RawUI.WindowSize.Width - 20, 60, 150)
 
     Write-Host -NoNewline ("${Name}`u{2026}".PadRight($padding))
 }
