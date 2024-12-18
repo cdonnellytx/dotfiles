ae895957ccf4b252d14ecb36af74b4032e4642be
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-17 18:10:51 -0600

    7zip

---
+++ home/.chezmoiscripts/windows/run_once_after-99-7zip.ps1
@@ -0,0 +1,17 @@
+#requires -version 7.0 -modules bootstrap.environment
+
+<#
+.SYNOPSIS
+Adds 7-Zip to user PATH so `7z` is available.
+#>
+[CmdletBinding(SupportsShouldProcess)]
+param()
+
+$7zipPath = Get-ItemPropertyValue -LiteralPath 'HKLM:\Software\7-Zip' -Name 'Path'
+if (!$7zipPath)
+{
+    Write-Error -Category InvalidOperation "7zip does not appear to be installed; cannot continue."
+    return
+}
+
+Add-PathVariable -Name PATH -Target User, Process -Value $7zipPath
