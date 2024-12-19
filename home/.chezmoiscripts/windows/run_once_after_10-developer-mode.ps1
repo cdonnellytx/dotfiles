627aa5a9141ba4c0cf55f7183ec74ecf992affb6
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-19 17:03:13 -0600

    Adding more initialization

---
+++ home/.chezmoiscripts/windows/run_once_after_10-developer-mode.ps1
@@ -0,0 +1,12 @@
+#requires -Version 7 -Modules bootstrap.registry -RunAsAdministrator
+
+<#
+.SYNOPSIS
+Enable Developer Mode.
+.LINK
+https://stackoverflow.com/questions/44158326/win10-how-to-activate-developer-mode-using-powershell-or-cmd-exe
+#>
+[CmdletBinding(SupportsShouldProcess)]
+param()
+
+Confirm-RegistryEntry -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock' -Name 'AllowDevelopmentWithoutDevLicense' -PropertyType DWORD -Value 1
