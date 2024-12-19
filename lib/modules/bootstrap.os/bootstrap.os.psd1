4361fc805d7277660277ccd997930dc7978dd808
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-19 17:01:50 -0600

    Administrator

--- lib/modules/bootstrap.os/bootstrap.os.psd1
+++ lib/modules/bootstrap.os/bootstrap.os.psd1
@@ -1,12 +1,13 @@
 @{
     RootModule = 'bootstrap.os.psm1'
-    ModuleVersion = '0.2.0'
+    ModuleVersion = '0.3.0'
     GUID = '2efd06db-67be-42c7-8088-08f35786a064'
     Author = 'Chris R. Donnelly'
     CompanyName = ''
     Copyright = '(c) Chris R. Donnelly. All rights reserved.'
     FunctionsToExport = @(
-        'Test-IsAdministrator'
+        'Test-IsAdministrator',
+        'Assert-IsAdminstrator'
     )
     CmdletsToExport = @()
     AliasesToExport = @()
--- lib/modules/bootstrap.os/bootstrap.os.psm1
+++ lib/modules/bootstrap.os/bootstrap.os.psm1
@@ -1,3 +1,6 @@
+using namespace System.Management.Automation
+using namespace System.Security.Principal
+
 Set-StrictMode -Version Latest
 
 <#
@@ -9,5 +12,26 @@ function Test-IsAdministrator
     [OutputType([bool])]
     param()
 
-    return ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+    return ([WindowsPrincipal] [WindowsIdentity]::GetCurrent()).IsInRole([WindowsBuiltInRole] "Administrator")
+}
+
+<#
+.SYNOPSIS
+Asserts whether the current user is running as administrator.
+#>
+function Assert-IsAdministrator
+{
+    [OutputType([void])]
+    param
+    (
+        # Specifies the assertion message.
+        [Parameter(Position = 0)]
+        [string] $Message = 'This command'
+    )
+
+    if (!(Test-IsAdministrator))
+    {
+        $exception = ([ScriptRequiresException]::new("${Message} cannot be run because it requires running as Administrator.  The current PowerShell session is not running as Administrator. Start PowerShell by using the Run as Administrator option, and then try running the script again."))
+        Write-Error -Category PermissionDenied -Exception:$exception -ErrorAction Stop
+    }
 }
