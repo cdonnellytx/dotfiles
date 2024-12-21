f77d9ab7aa83942fa9e5e3ab1d5b65c1f280f3be
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-20 22:24:13 -0600

    Make sudo work independently

--- home/.chezmoiscripts/windows/run_once_after_10-developer-mode.ps1
+++ home/.chezmoiscripts/windows/run_once_after_10-developer-mode.ps1
@@ -2,7 +2,7 @@
 
 <#
 .SYNOPSIS
-Enable Developer Mode and related settings.
+Enable Developer Mode.
 .LINK
 https://stackoverflow.com/questions/44158326/win10-how-to-activate-developer-mode-using-powershell-or-cmd-exe
 #>
@@ -11,13 +11,3 @@ param()
 
 # Developer mode
 Confirm-RegistryEntry -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock' -Name 'AllowDevelopmentWithoutDevLicense' -PropertyType DWORD -Value 1
-
-# Sudo (Windows 11 24H2 and later)
-if ((Get-ChezmoiData).chezmoi.windowsVersion.currentBuild -ge 26052)
-{
-    # 0 - disabled
-    # 1 - new window
-    # 2 - with input disabled
-    # 3 - inline
-    Confirm-RegistryEntry -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Sudo' -Name 'Enabled' -PropertyType DWORD -Value 3
-}
