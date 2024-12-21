d9e2f04c58d9cd9496eb533d85cba50c0cabcbf5
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-20 22:13:25 -0600

    Enable builtin Windows sudo

--- home/.chezmoiscripts/windows/run_once_after_10-developer-mode.ps1
+++ home/.chezmoiscripts/windows/run_once_after_10-developer-mode.ps1
@@ -1,12 +1,19 @@
-#requires -Version 7 -Modules bootstrap.registry -RunAsAdministrator
+#requires -Version 7 -Modules bootstrap.chezmoi, bootstrap.registry -RunAsAdministrator
 
 <#
 .SYNOPSIS
-Enable Developer Mode.
+Enable Developer Mode and related settings.
 .LINK
 https://stackoverflow.com/questions/44158326/win10-how-to-activate-developer-mode-using-powershell-or-cmd-exe
 #>
 [CmdletBinding(SupportsShouldProcess)]
 param()
 
+# Developer mode
 Confirm-RegistryEntry -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock' -Name 'AllowDevelopmentWithoutDevLicense' -PropertyType DWORD -Value 1
+
+# Sudo (Windows 11 24H2 and later)
+if ((Get-ChezmoiData).chezmoi.windowsVersion.currentBuild -ge 26052)
+{
+    Confirm-RegistryEntry -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Sudo' -Name 'Enabled' -PropertyType DWORD -Value 3
+}
