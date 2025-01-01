23f28dfcc1da46fc90958049ee959c49882f5118
Chris Donnelly <cdonnelly@69bytes.com>
2024-12-31 19:25:51 -0600

    Minor tweak

--- home/.chezmoiscripts/windows/run_once_before_10-lang.ps1
+++ home/.chezmoiscripts/windows/run_once_before_10-lang.ps1
@@ -7,13 +7,9 @@ Set CLI LANG variables.
 [CmdletBinding(SupportsShouldProcess)]
 param()
 
-process
-{
-    # POSIX LANG
-    Set-EnvironmentVariable 'LANG' 'en_US.UTF-8' -Target User @PSBoundParameters
-    Set-EnvironmentVariable 'LC_ALL' 'en_US.UTF-8' -Target User @PSBoundParameters
+# POSIX LANG
+Set-EnvironmentVariable 'LANG' 'en_US.UTF-8' -Target User @PSBoundParameters
+Set-EnvironmentVariable 'LC_ALL' 'en_US.UTF-8' -Target User @PSBoundParameters
 
-    # Less
-    Set-EnvironmentVariable 'LESSCHARSET' 'UTF-8' -Target User @PSBoundParameters
-
-}
+# Less
+Set-EnvironmentVariable 'LESSCHARSET' 'UTF-8' -Target User @PSBoundParameters
